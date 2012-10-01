f1e7632a98e1a58d8bee6de004da1fa73ab4d068
Federico Bond <federicobond@gmail.com>
2012-10-01 14:57:41 -0300

    Router funcionando

--- js/base.js
+++ js/base.js
@@ -1,50 +1,73 @@
+window.App = {}
+
+App.Flight = Backbone.Model.extend();
+
+App.FlightList = Backbone.Collection.extend({
+    model: App.Flight,
+
+    fetch: function() {
+        var list = [
+            new Flight({ name: "Flight #1" }),
+            new Flight({ name: "Flight #2" }),
+            new Flight({ name: "Flight #3" })
+        ];
+        return list;
+    }
+});
+
+App.Router = Backbone.Router.extend({
+    routes: {
+        "" : "home",
+        "about": "about",
+        "search/:query": "search"
+    },
+
+    home: function() {
+        this.switchPage("home")
+    },
+
+    about: function() {
+        this.switchPage("about")
+    },
+
+    search: function(query) {
+        alert("query: " + query);
+    },
+
+    switchPage: function(pageName) {
+        $(".page.current").removeClass("current");
+        $("#page-" + pageName).addClass("current");
+    },
+
+    defaultRoute: function(actions) {
+        alert(actions);
+    }
+
+});
+
+App.FlightView = Backbone.View.extend({
+    template: $("template-flight").html(),
+
+    render: function() {
+        this.$el.html(Mustache.render(this.template, { name: "Flight #1234" }));
+        return this;
+    }
+
+});
+
+App.AppView = Backbone.View.extend({
+
+    el: $("#app"),
+
+    initialize: function() {
+        alert("initialized appview");
+    }
+
+});
+
+
 $(function() {
-
-    var Workspace = Backbone.Router.extend({
-        routes: {
-            "/about": "about",
-            "/search/:query": "search"
-        },
-
-        about: function() {
-            alert("About!");
-            $(".container").html("Hello");
-        }
-
-    });
-
-    var Flight = Backbone.Model.extend();
-
-    var Flight = new Flight();
-
-    var FlightList = Backbone.Collection.extend({
-        model: Flight
-    });
-
-    var Flights = new FlightList;
-
-    var FlightView = Backbone.View.extend({
-        template: $("template-flight").html(),
-
-        render: function() {
-            this.$el.html(Mustache.render(this.template, { name: "Flight #1234" }));
-            return this;
-        }
-
-    });
-
-    var AppView = Backbone.View.extend({
-
-        el: $("app"),
-
-        initialize: function() {
-
-        }
-
-    });
-
-    var App = new AppView;
-
-    // Backbone.history.start();
+    new App.Router();
+    Backbone.history.start();
 
 });
